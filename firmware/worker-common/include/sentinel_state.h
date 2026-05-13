3c1a406146f010880b3b5aaf63b69e93df17039a
avainfo <avainfoconseils@gmail.com>
2026-05-13 01:56:17 +0100

    chore(header): remove personal header

--- firmware/worker-common/include/sentinel_state.h
+++ firmware/worker-common/include/sentinel_state.h
@@ -1,14 +1,3 @@
-/*
- * Ava Info Conseils
- *
- * Author: Antonin Do Souto
- * Contact: antonindosouto@gmail.com
- *
- * Copyright (c) 2026 Antonin Do Souto
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 enum SentinelState {
 	INIT,
 	NOMINAL,
