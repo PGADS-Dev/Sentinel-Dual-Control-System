8a8a2dbcee8a915f4a28421892b6c7f5beef780a
avainfo <avainfoconseils@gmail.com>
2026-05-13 01:53:02 +0100

    feat(state): add 4 states for the workers

---
+++ firmware/worker-common/include/sentinel_state.h
@@ -0,0 +1,17 @@
+/*
+ * Ava Info Conseils
+ *
+ * Author: Antonin Do Souto
+ * Contact: antonindosouto@gmail.com
+ *
+ * Copyright (c) 2026 Antonin Do Souto
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+enum SentinelState {
+	INIT,
+	NOMINAL,
+	DEGRADED,
+	FAIL_SAFE
+};
