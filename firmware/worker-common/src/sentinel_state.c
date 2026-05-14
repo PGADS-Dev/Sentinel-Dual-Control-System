b1181de7a93584b2228ccafc4804ef48620b410a
avainfo <avainfoconseils@gmail.com>
2026-05-14 03:06:42 +0100

    feat(states): add function that changes the state based on an event

--- firmware/worker-common/src/sentinel_state.c
+++ firmware/worker-common/src/sentinel_state.c
@@ -1,7 +1,7 @@
 #include "sentinel_state.h"
 #include "sentinel_fault.h"
 
-const char* sentinel_state_to_string(SentinelState state) {
+const char *sentinel_state_to_string(SentinelState state) {
 	switch (state) {
 	case SENTINEL_STATE_INIT:
 		return "INIT";
@@ -16,7 +16,7 @@ const char* sentinel_state_to_string(SentinelState state) {
 	}
 }
 
-const char* sentinel_event_to_string(SentinelEvent event) {
+const char *sentinel_event_to_string(SentinelEvent event) {
 	switch (event) {
 	case SENTINEL_EVENT_INVALID:
 		return "INVALID";
@@ -39,7 +39,7 @@ const char* sentinel_event_to_string(SentinelEvent event) {
 	}
 }
 
-const char* sentinel_fault_to_string(SentinelFault fault) {
+const char *sentinel_fault_to_string(SentinelFault fault) {
 	switch (fault) {
 	case SENTINEL_FAULT_HEARTBEAT_LOST:
 		return "HEARTBEAT_LOST";
@@ -49,5 +49,38 @@ const char* sentinel_fault_to_string(SentinelFault fault) {
 		return "INCOHERENT_PEER_STATE";
 	default:
 		return "UNKNOWN_FAULT";
+	}
+}
+
+SentinelState sentinel_next_state(SentinelState current, SentinelEvent event) {
+	switch (current) {
+	case SENTINEL_STATE_INIT:
+		if (event == SENTINEL_EVENT_STARTUP_COMPLETE) {
+			return SENTINEL_STATE_NOMINAL;
+		} else if (event == SENTINEL_EVENT_COMMUNICATION_LOST) {
+			return SENTINEL_STATE_FAIL_SAFE;
+		}
+		return SENTINEL_STATE_INIT;
+	case SENTINEL_STATE_NOMINAL:
+		if (event == SENTINEL_EVENT_HEARTBEAT_WARNING || event == SENTINEL_EVENT_INCOHERENT_PEER_STATE) {
+			return SENTINEL_STATE_DEGRADED;
+		} else if (event == SENTINEL_EVENT_COMMUNICATION_LOST || event == SENTINEL_EVENT_FAULT_ESCALATED) {
+			return SENTINEL_STATE_FAIL_SAFE;
+		}
+		return SENTINEL_STATE_NOMINAL;
+	case SENTINEL_STATE_DEGRADED:
+		if (event == SENTINEL_EVENT_FAULT_CLEARED) {
+			return SENTINEL_STATE_NOMINAL;
+		} else if (event == SENTINEL_EVENT_FAULT_ESCALATED || event == SENTINEL_EVENT_COMMUNICATION_LOST) {
+			return SENTINEL_STATE_FAIL_SAFE;
+		}
+		return SENTINEL_STATE_DEGRADED;
+	case SENTINEL_STATE_FAIL_SAFE:
+		if (event == SENTINEL_EVENT_MANUAL_RESET) {
+			return SENTINEL_STATE_INIT;
+		}
+		return SENTINEL_STATE_FAIL_SAFE;
+	default:
+		return SENTINEL_STATE_FAIL_SAFE;
 	}
 }
